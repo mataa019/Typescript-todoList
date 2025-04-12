67db05f9a1d2ced155cd04be21191ab3960edf1b
mataa019 <johnnymataa019@gmail.com>
2025-04-12 13:37:49 +0200

    install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <main className="py-8 font-bold h-screen">
-      <h1 className="text-4xl text-center font-mono">
+      <h1 className="text-4xl text-center">
         Mataa here 
       </h1>
     </main>
---
+++ src/data/todo.ts
@@ -0,0 +1,17 @@
+export const dummyTodos = [
+    {
+        id:1,
+        title:"Like us for more updates",
+        completed: false
+    },
+    {
+        id:2,
+        title:"Subscribe for more updates",
+        completed: false
+    },
+    {
+        id:3,
+        title:"Share with your friends",
+        completed: false
+    }
+]
