a54c462ad6be3b197b7a4b86eff6cb8eff81f8c0
mataa019 <johnnymataa019@gmail.com>
2025-04-13 20:31:13 +0200

    implementing the todoList Model

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { dummyTodos } from "./data/todo"
 function App() {
 
   return (
-    <main className="py-8 font-mono h-screen">
-      <h1 className="text-4xl text-center">
-        Mataa here 
+    <main className="py-8  h-screen space-y-5">
+      <h1 className="text-4xl text-center font-bold">
+        Todo List
       </h1>
-      <div className="max-w-lg mx-auto ">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
          <div className="space-y-4 ">
             {dummyTodos.map((todo) => (
               <TodoItem todo={todo}/>
