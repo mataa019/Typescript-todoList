f17b2a814c262926491d47d170452c24245df152
mataa019 <johnnymataa019@gmail.com>
2025-04-22 10:02:29 +0200

    setTodoCompleted

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { dummyTodos } from "./data/todo"
 
 function App() {
 
+  function setTodocompleted(id: number, completed: boolean) {
+
+  }
   return (
     <main className="py-8  h-screen space-y-5">
       <h1 className="text-4xl text-center font-bold">
@@ -11,7 +14,9 @@ function App() {
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
          <div className="space-y-4 ">
             {dummyTodos.map((todo) => (
-              <TodoItem todo={todo}/>
+              <TodoItem todo={todo}
+               onCompletedChange={setTodocompleted}
+              />
             ))}
          </div>
       </div>
--- src/components/todoItem.tsx
+++ src/components/todoItem.tsx
@@ -5,12 +5,15 @@ interface TodoItemProps {
     onCompletedChange: (id: number, completed: boolean) => void;
 }
  
- function TodoItem( {todo}: TodoItemProps) {
+ function TodoItem( {todo, onCompletedChange}: TodoItemProps) {
      return(
         <>
             <div>
-               <label className=" flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover-bg-slate-50">
-                  <input type="checkbox" className="scale-125"/>
+               <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover-bg-slate-50">
+                  <input type="checkbox" 
+                  checked={todo.completed}
+                  onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
+                  className="scale-125"/>
                   <span className={todo.completed ? "line-through text-gray-400": " "}> {todo.title}</span>
                </label>
             </div>
