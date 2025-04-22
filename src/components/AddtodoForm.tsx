91d52eac763a2bcf85e8190f51ee028d4b56e915
mataa019 <johnnymataa019@gmail.com>
2025-04-22 10:39:31 +0200

    setTodoCompleted

--- src/components/AddtodoForm.tsx
+++ src/components/AddtodoForm.tsx
@@ -6,8 +6,8 @@ function AddTodoList (){
                 <input type="text" placeholder="Enter todo" 
                  className="rounded-s-md grow border border-gray-400 p-2"
                 />
-                <button type="submit">Add</button>
             </form>
+            <button type="submit">Add</button>
         </div>
     )
 }
